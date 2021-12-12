173712accf8a1e2e3029d4f9b830adf7761d5bda
Eric Jameson <e-jameson@bethel.edu>
2021-12-12 15:32:17 -0600

    2021 day 11

---
+++ aoc2021/aoc/day11/part1.py
@@ -0,0 +1,36 @@
+# Advent of Code - Day 11 - Part One
+
+def result(data):
+
+    octs = {}
+
+    def neighbours(x, y):
+        r = []
+        coords = [(x + 1, y + 1), (x + 1, y), (x + 1, y - 1), (x, y + 1), (x - 1, y - 1), (x - 1, y), (x - 1, y + 1), (x, y - 1)]
+        for c in coords:
+            oct = octs.get(c, 0)
+            if oct:
+                r.append(c)
+        return r
+
+
+    for x, line in enumerate(data):
+        for y, energy in enumerate(line):
+            octs[(x, y)] = int(energy)
+
+    flashes = 0
+    for step in range(100):
+        for o in octs:
+            octs[o] += 1
+        flashing = {o for o in octs if octs[o] > 9}
+
+        while flashing:
+            f = flashing.pop()
+            octs[f] = 0
+            flashes += 1
+            for n in neighbours(*f):
+                octs[n] += 1
+                if octs[n] > 9:
+                    flashing.add(n)
+
+    return flashes
