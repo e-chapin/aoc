1e9d34e19f175a11f99f1cb128939dc6dd79fe9c
Eric Jameson <e-jameson@bethel.edu>
2021-12-17 09:38:43 -0600

    2021 day 17

--- aoc2021/aoc/day17/part1.py
+++ aoc2021/aoc/day17/part1.py
@@ -24,27 +24,27 @@ def step(coord, velocity):
 
 
 def result(data):
-    # # target area: x=211..232, y=-124..-69
-    # targets = defaultdict(int)
-    # for x in range(211, 233):
-    #     for y in range(69, 125):
-    #         targets[(x,-1*y)] = 1
-    #
-    # max_y = -1
-    # for x in range(125):
-    #     for y in range(125):
-    #         coord = (0, 0)
-    #         velocity = (x, y)
-    #         apex = -1
-    #         while True:
-    #             coord, velocity = step(coord, velocity)
-    #             if coord[1] > apex:
-    #                 apex = coord[1]
-    #             if coord in targets.keys():
-    #                 if apex > max_y:
-    #                     max_y = apex
-    #                 break
-    #             if coord[1] < -124:
-    #                 break
+    # target area: x=211..232, y=-124..-69
+    targets = defaultdict(int)
+    for x in range(211, 233):
+        for y in range(69, 125):
+            targets[(x,-1*y)] = 1
 
-    return -1
+    max_y = -1
+    for x in range(125):
+        for y in range(125):
+            coord = (0, 0)
+            velocity = (x, y)
+            apex = -1
+            while True:
+                coord, velocity = step(coord, velocity)
+                if coord[1] > apex:
+                    apex = coord[1]
+                if coord in targets.keys():
+                    if apex > max_y:
+                        max_y = apex
+                    break
+                if coord[1] < -124:
+                    break
+
+    return max_y
